4e21c68fe2577e8e824358876530fc6d688a20ee
Tom Soukup <tosinek@gmail.com>
2023-11-18 11:53:18 +0100

    show upcoming events

--- src/routes/[email]/+page.svelte
+++ src/routes/[email]/+page.svelte
@@ -2,6 +2,7 @@
   import { onMount } from "svelte";
   import { allCourses } from "./courses";
   import type { PageData } from "./$types";
+  import CourseCard from "./CourseCard.svelte";
   export let data: PageData;
 
   /**
@@ -70,52 +71,11 @@
     }
     return course;
   });
-
-  // format date as YYYYMMDDTMMHHSSZ
-  const formatDate = (date) => {
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, "0");
-    const day = date.getDate().toString().padStart(2, "0");
-    const hours = date.getUTCHours().toString().padStart(2, "0");
-    const minutes = date.getUTCMinutes().toString().padStart(2, "0");
-    return `${year}${month}${day}T${hours}${minutes}00Z`;
-  };
-
-  const addToCalendar = (item, returnTitle = false) => {
-    const location = "DEPO2015, Pilsen, Czech Republic";
-    const description = item.name + " with " + item.teachers;
-
-    const title =
-      "LHHC 2023: " +
-      (item.type === "Party"
-        ? item.name
-        : item.type + (item.level ? " " + item.level : "") + " - " + item.name);
-
-    if (returnTitle) return title;
-
-    return `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(
-      title
-    )}&location=${encodeURIComponent(location)}&dates=${formatDate(
-      item.start
-    )}/${formatDate(item.end)}&details=${encodeURIComponent(description)}`;
-  };
-
-  $: getLocation = (timeSlot) => {
-    const no = timeSlot.number?.match(/\d{2}/);
-    if (no) {
-      return locations[parseInt(no)];
-    } else if (timeSlot.name === "Saturday party") {
-      return locations[locations.length - 1];
-    } else if (timeSlot.name === "Friday party") {
-      return locations[locations.length - 2];
-    }
-    return "location to be announced";
-  };
 </script>
 
 <h1>Your schedule</h1>
 {#if data.email}
-  ({data.email})
+  <div class="email">({data.email})</div>
 {/if}
 
 {#if loading}
@@ -127,6 +87,20 @@
 {:else if !userData.length}
   <p>No data</p>
 {:else}
+  <h2>Upcoming events:</h2>
+  <div class="cards">
+    {#each allCoursesWithRegistrations.filter((f) => f.registered && f.end.getTime() > Date.now()) as timeSlot}
+      <CourseCard {timeSlot} {partyProgram} {locations} />
+    {/each}
+  </div>
+
+  <h2>Past events:</h2>
+  <div class="cards">
+    {#each allCoursesWithRegistrations.filter((f) => f.registered && f.end.getTime() <= Date.now()) as timeSlot}
+      <CourseCard {timeSlot} {partyProgram} {locations} />
+    {/each}
+  </div>
+
   <div class="payment" class:paid={userData[22]}>
     {#if userData[22]}
       Your order is paid.
@@ -134,113 +108,33 @@
       Your payment hasn't been processed yet.
     {/if}
   </div>
-
-  <div class="cards">
-    {#each allCoursesWithRegistrations.filter((f) => f.registered) as timeSlot}
-      <div class="card">
-        <div class="course">
-          <div class="course-name">{timeSlot.number || ""} {timeSlot.name}</div>
-          {#if timeSlot.type !== "Party"}
-            <div class="course-type">
-              {timeSlot.level}
-              {timeSlot.type}
-            </div>
-          {/if}
-          <div class="course-type">
-            with {timeSlot.teachers}
-          </div>
-
-          {#if timeSlot.type === "Party"}
-            <div class="course-type" style="margin-top: 10px;">
-              <b>Program:</b> <br />
-              {#if timeSlot.day === "Friday"}
-                {#each partyProgram[0] as i}
-                  {i}<br />
-                {/each}
-              {/if}
-              {#if timeSlot.day === "Saturday"}
-                {#each partyProgram[1] as i}
-                  {i}<br />
-                {/each}
-              {/if}
-            </div>
-          {/if}
-        </div>
-
-        <div class="info">
-          {timeSlot.day}
-          {timeSlot.time}
-          {#if timeSlot.registered === "LEADER" || timeSlot.registered === "FOLLOWER"}
-            as {timeSlot.registered}
-          {/if}
-          <div class="location">@{getLocation(timeSlot)}</div>
-
-          <div class="calendar">
-            <a href={addToCalendar(timeSlot)} target="_blank"
-              >Add to your Google calendar</a
-            >
-          </div>
-        </div>
-      </div>
-    {/each}
-  </div>
 {/if}
 
 <style>
   h1 {
     margin-block: 0;
+    text-align: center;
     font-size: 1.5rem;
-    justify-self: flex-start;
+    /* justify-self: flex-start; */
+  }
+  h2 {
+    font-size: 1.2rem;
+    margin: 2rem 0 1rem;
+  }
+  h1,
+  h2,
+  .email {
+    color: rgba(0, 0, 0, 0.7);
+    text-align: center;
   }
   .cards {
     --border-radius: 10px;
     display: grid;
-    margin-block: 2rem;
+    margin-block: 0.5rem;
     gap: 2rem 2rem;
     grid-template-columns: minmax(200px, 450px);
   }
-  .card {
-    background-color: rgb(253, 244, 244);
-    box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.2);
-    display: grid;
-    border-radius: var(--border-radius, 5px);
-  }
-  .course,
-  .info {
-    padding-inline: 1rem;
-    padding-block: 1rem;
-    text-align: center;
-  }
-  .course {
-    border-radius: var(--border-radius, 5px) var(--border-radius, 5px) 0 0;
-    background-color: rgb(255, 190, 183);
-    background-color: aliceblue;
-    font-family: monospace;
-  }
-  .info {
-    border-radius: 0 0 var(--border-radius, 5px) var(--border-radius, 5px);
-    background-color: rgb(43, 144, 226);
-    color: white;
-  }
-  .course-name {
-    font-size: 1.3rem;
-  }
-  .course-type {
-    font-size: 0.9rem;
-    opacity: 0.8;
-    margin-top: 4px;
-    font-weight: 700;
-  }
-  .location {
-    margin-top: 10px;
-  }
-  .calendar {
-    margin-top: 10px;
-    font-size: 0.8em;
-  }
-  .calendar a {
-    color: white;
-  }
+
   .payment {
     margin-block: 2rem;
     padding: 1rem;
